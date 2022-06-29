99e0aa984de106fd05da9f8fd00638e01c9c722f
roddsvosf <rodrigo.soares@osf.digital>
2022-06-29 09:59:20 -0300

    Reset value, add decimal

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const clearBtn = document.getElementById('clear-btn');
 function sendNumberValue (number){
     // If current display value is 0, replace it, if not add number
     const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
-    
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;   
+}
+
+function addDecimal() {
+    // If no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
@@ -16,6 +22,14 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue());
+        inputBtn.addEventListener('click', () => addDecimal());
     }
 });
+
+// Reset display to default
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
